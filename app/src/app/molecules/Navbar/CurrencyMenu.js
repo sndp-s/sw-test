5c3e3f922139a767577edcc63c014cd1a6c407da
Sandeep Sharma <sandeeptech8@gmail.com>
2022-11-02 23:46:30 +0530

    init
    initial setup.

---
+++ app/src/app/molecules/Navbar/CurrencyMenu.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class CurrencyMenu extends React.Component {
+
+  render() {
+    return (
+      <ul class="">
+        CurrencyMenu
+      </ul>
+    );
+  }
+}
+
+export default CurrencyMenu;

de32222fbed01ea0efaa7881c8e052715fbc801b
Sandeep Sharma <sandeeptech8@gmail.com>
2022-12-03 17:34:24 +0530

    add <CartMenu/>

--- app/src/app/molecules/menu-list/CartMenu.js
+++ app/src/app/molecules/menu-list/CartMenu.js
@@ -1,13 +1,77 @@
 import React from 'react';
+import './CartMenu.scss';
+import EmptyCartBlack from '../../../assets/outlined/empty-cart-black.svg';
+import Dropdown from '../../atoms/Dropdown/Dropdown';
+import ScrollView from '../../atoms/ScrollView/ScrollView';
+import CartItem from '../../atoms/CartItem/CartItem';
+import CartContext from '../../state/contexts/CartContext';
+
+
+class Menu extends React.Component {
+  static contextType = CartContext;
+
+  render() {
+    const { cart, normalisedCart } = this.context;
+    return(
+      <div className="menu">
+        <div className="heading">
+          <p className="heading__text">My Bag, {cart.length ?? 0} items</p>
+        </div>
+        <div className="product-list">
+          <ScrollView hideScroll>
+            {normalisedCart.map((product, index) => (
+              <CartItem
+                item={product}
+                key={index}
+              />
+            ))}
+          </ScrollView>
+        </div>
+      </div>
+    );
+  }
+}
+
 
 
 class CartMenu extends React.Component {
+  static contextType = CartContext;
+
+  constructor() {
+    super();
+    this.onClose = this.onClose.bind(this);
+    this.handleOnToggle = this.handleOnToggle.bind(this);
+    this.state = {
+      isOpen: false,
+    }
+  }
+
+  handleOnToggle(e) {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    }) 
+  }
+
+  onClose(e) {
+    this.setState({
+      isOpen: false
+    })
+  }
 
   render() {
+    const { cart } = this.context;
+
     return (
-      <div className="">
-        CartMenu
-      </div>
+        <Dropdown
+          isOpen={this.state.isOpen}
+          onClose={this.onClose}
+          render={<Menu />}
+        >
+          <button className='head' onClick={this.handleOnToggle}>
+            <img src={EmptyCartBlack} alt="cart icon" />
+            <p className="cart-item-count">{cart.length ?? 0}</p>
+          </button>
+        </Dropdown>
     );
   }
 }
--- app/src/app/molecules/menu-list/MenuList.js
+++ app/src/app/molecules/menu-list/MenuList.js
@@ -10,7 +10,7 @@ class MenuList extends React.Component {
     return (
       <div className='list'>
         <CurrencyMenu />
-        {/* <CartMenu /> */}
+        <CartMenu />
       </div>
     );
   }

e140d0727f30e8864fdb65792c22fd3713bbe247
Sandeep Sharma <sandeeptech8@gmail.com>
2022-12-14 00:58:54 +0530

    feat: variants, sizes

--- app/src/app/atoms/Text/Text.js
+++ app/src/app/atoms/Text/Text.js
@@ -1,25 +1,43 @@
 import React from 'react';
+import './Text.scss';
 import PropTypes from 'prop-types';
 
 class Text extends React.Component {
   render() {
-    const { children, span, variant } = this.props;
+    let { children, span, variant, className, size } = this.props;
+
+    className += ['txt', `txt-${size}`].join(' ');
+
+    if(span) {
+      return (
+        <span className={className}>
+          {children}
+        </span>
+      )
+    }
 
     switch(variant) {
-
-    }
-    
-    
-    return (
-      <p>{children}</p>
-    );
+      case 'h1': return <h1 className={className}>{children}</h1>
+      case 'h2': return <h2 className={className}>{children}</h2>
+      case 'h3': return <h3 className={className}>{children}</h3>
+      case 'p': return <p className={className}>{children}</p>
+      default: return <p className={className}>{children}</p>
+    }   
   }
 }
 
-export default Text;
+
+Text.defaultProps = {
+  className: '',
+  variant: 'p',
+  size: 's',
+}
 
 Text.propTypes = {
   chidren: PropTypes.node.isRequired,
   variant: PropTypes.oneOf(['h1', 'h2', 'h3', 'p']),
-  weight: PropTypes
+  className: PropTypes.string,
+  size: PropTypes.oneOf(['xs', 's', 'm', 'l', 'xl']),
 }
+
+export default Text;

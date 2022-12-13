fc334fe0d2048c171263afd7aed25e2d3d13ac07
Sandeep Sharma <sandeeptech8@gmail.com>
2022-12-14 00:55:10 +0530

    Feat: add /cart route
    - Add /cart route
    - Remove unnecessary brackets

--- app/src/App.js
+++ app/src/App.js
@@ -5,33 +5,27 @@ import './App.scss';
 import Header from './app/organisms/header/Header';
 import ProductListingPage from './app/organisms/product-listing-page/ProductListingPage';
 import ProductDisplayPage from './app/organisms/product-display-page/ProductDisplayPage';
+import CartPage from './app/organisms/cart-page/CartPage';
 
 class App extends React.Component {
   render() {
-    // return (
-    //   <div className='App'>
-    //     <Header />
-    //     <div className='page--container'>
-    //       <main className='main'>
-    //         <ProductListingPage />
-    //       </main>
-    //     </div>
-    //   </div>
-    // );
     return (
       <div className='App'>
         <Router>
-          <Header />
+          <Header/>
             <div className='page--container'>
               <Switch>
                 <Route path='/' exact>
-                  {<ProductListingPage/>}
+                  <ProductListingPage/>
                 </Route>
                 <Route path='/category/:categoryName' exact>
-                  {<ProductListingPage/>}
+                  <ProductListingPage/>
                 </Route>
                 <Route path='/product/:productId' exact>
-                  {<ProductDisplayPage/>}
+                  <ProductDisplayPage/>
+                </Route>
+                <Route path='/cart' exact>
+                  <CartPage/>
                 </Route>
               </Switch>
             </div>

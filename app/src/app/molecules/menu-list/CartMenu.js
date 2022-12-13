8fd070df87b580a827dbefd9d70de72e55feb06e
Sandeep Sharma <sandeeptech8@gmail.com>
2022-12-14 01:06:34 +0530

    Break out <Menu/> (of <CartMenu/>) into separate files and fetch products  in there.
    - Move Menu to its own files.
    - Fetch the product in Menu itself

--- app/src/app/molecules/menu-list/CartMenu.js
+++ app/src/app/molecules/menu-list/CartMenu.js
@@ -2,37 +2,8 @@ import React from 'react';
 import './CartMenu.scss';
 import EmptyCartBlack from '../../../assets/outlined/empty-cart-black.svg';
 import Dropdown from '../../atoms/Dropdown/Dropdown';
-import ScrollView from '../../atoms/ScrollView/ScrollView';
-import CartItem from '../../atoms/CartItem/CartItem';
 import CartContext from '../../state/contexts/CartContext';
-
-
-class Menu extends React.Component {
-  static contextType = CartContext;
-
-  render() {
-    const { cart, normalisedCart } = this.context;
-    return(
-      <div className="menu">
-        <div className="heading">
-          <p className="heading__text">My Bag, {cart.length ?? 0} items</p>
-        </div>
-        <div className="product-list">
-          <ScrollView hideScroll>
-            {normalisedCart.map((product, index) => (
-              <CartItem
-                item={product}
-                key={index}
-              />
-            ))}
-          </ScrollView>
-        </div>
-      </div>
-    );
-  }
-}
-
-
+import Menu from './Menu';
 
 class CartMenu extends React.Component {
   static contextType = CartContext;
@@ -62,16 +33,16 @@ class CartMenu extends React.Component {
     const { cart } = this.context;
 
     return (
-        <Dropdown
-          isOpen={this.state.isOpen}
-          onClose={this.onClose}
-          render={<Menu />}
-        >
-          <button className='head' onClick={this.handleOnToggle}>
-            <img src={EmptyCartBlack} alt="cart icon" />
-            <p className="cart-item-count">{cart.length ?? 0}</p>
-          </button>
-        </Dropdown>
+      <Dropdown
+        isOpen={this.state.isOpen}
+        onClose={this.onClose}
+        render={<Menu/>}
+      >
+        <button className='head' onClick={this.handleOnToggle}>
+          <img src={EmptyCartBlack} alt="cart icon" />
+          <p className="cart-item-count">{cart.length ?? 0}</p>
+        </button>
+      </Dropdown>
     );
   }
 }

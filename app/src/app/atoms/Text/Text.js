60f2b5fb0cc1fb8ccf77b98b7fcb3d95b5aa28e0
Sandeep Sharma <sandeeptech8@gmail.com>
2022-12-17 13:27:04 +0530

    fix: custom className prop not getting applied

--- app/src/app/atoms/Text/Text.js
+++ app/src/app/atoms/Text/Text.js
@@ -6,22 +6,22 @@ class Text extends React.Component {
   render() {
     let { children, span, variant, className, size } = this.props;
 
-    className += ['txt', `txt-${size}`].join(' ');
+    const classes = ['txt', `txt-${size}`, className].join(' ');
 
     if(span) {
       return (
-        <span className={className}>
+        <span className={classes}>
           {children}
         </span>
       )
     }
 
     switch(variant) {
-      case 'h1': return <h1 className={className}>{children}</h1>
-      case 'h2': return <h2 className={className}>{children}</h2>
-      case 'h3': return <h3 className={className}>{children}</h3>
-      case 'p': return <p className={className}>{children}</p>
-      default: return <p className={className}>{children}</p>
+      case 'h1': return <h1 className={classes}>{children}</h1>
+      case 'h2': return <h2 className={classes}>{children}</h2>
+      case 'h3': return <h3 className={classes}>{children}</h3>
+      case 'p': return <p className={classes}>{children}</p>
+      default: return <p className={classes}>{children}</p>
     }   
   }
 }

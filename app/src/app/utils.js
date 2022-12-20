6eaa29a20f1b15dfc496018880df135aee79db1e
Sandeep Sharma <sandeeptech8@gmail.com>
2022-12-20 13:36:54 +0530

    add getEventCoords()

--- app/src/app/utils.js
+++ app/src/app/utils.js
@@ -1,4 +1,25 @@
+export function getEventCoords(ev, targetSelector) {
+  let boxInfo;
+
+  const path = ev.nativeEvent.composedPath();
+  const target = targetSelector
+    ? path.find((element) => element.matches?.(targetSelector))
+    : null;
+  if (target) {
+    boxInfo = target.getBoundingClientRect();
+  } else {
+    boxInfo = ev.target.getBoundingClientRect();
+  }
+
+  return {
+    x: boxInfo.x,
+    y: boxInfo.y,
+    width: boxInfo.width,
+    height: boxInfo.height,
+    detail: ev.detail,
+  };
+}
+
 export function shortId() {
   return `_${Math.random().toString(36).substring(2, 11)}`;
 }
-

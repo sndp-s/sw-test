f451c27dce43c3c43e828fa526849586439319c2
Sandeep Sharma <sandeeptech8@gmail.com>
2023-01-07 15:25:54 +0530

    add font-weight support

--- app/src/app/atoms/Text/Text.js
+++ app/src/app/atoms/Text/Text.js
@@ -4,9 +4,9 @@ import PropTypes from 'prop-types';
 
 class Text extends React.Component {
   render() {
-    let { children, span, variant, className, size } = this.props;
+    let { children, span, variant, className, size, weight } = this.props;
 
-    const classes = ['txt', `txt-${size}`, className].join(' ');
+    const classes = ['txt', `txt--size-${size}`, `txt--weight-${weight}`, className].join(' ');
 
     if(span) {
       return (
@@ -31,6 +31,7 @@ Text.defaultProps = {
   className: '',
   variant: 'p',
   size: 's',
+  weight: 400,
 }
 
 Text.propTypes = {
@@ -38,6 +39,7 @@ Text.propTypes = {
   variant: PropTypes.oneOf(['h1', 'h2', 'h3', 'p']),
   className: PropTypes.string,
   size: PropTypes.oneOf(['xs', 's', 'm', 'l', 'xl']),
+  weight: PropTypes.oneOf([300, 400, 500, 600, 700])
 }
 
 export default Text;

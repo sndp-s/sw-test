c23f54601a615e35643750d36258cbe731d0c98a
Sandeep Sharma <sandeeptech8@gmail.com>
2023-01-08 15:26:46 +0530

    remove variants

--- app/src/app/atoms/Button/Button.js
+++ app/src/app/atoms/Button/Button.js
@@ -2,52 +2,34 @@ import React from 'react';
 import './Button.scss';
 import PropTypes from 'prop-types';
 
-class Button extends React.Component {
-  render() {
-    const { children, onClick, type, className, variant, color } = this.props;
-
-    let variantClass = '';
-    switch (variant) {
-      case 's': variantClass = 'btn--s'; break;
-      case 'l': variantClass = 'btn--l'; break;
-      default: variantClass = 'btn--s';
+const Button = React.forwardRef(
+  ({ children, onClick, type, className }, forwadedRef) => {
+  class Button extends React.Component {
+    render() {
+      const classes = ['btn', className];
+      return (
+        <button
+          type={type}
+          ref={forwadedRef}
+          onClick={onClick}
+          className={classes.join(' ')}
+        >
+          {children}
+        </button>
+      );
     }
-
-    let colorClass = '';
-    switch (color) {
-      case 'white': colorClass += 'btn--color-white'; break;
-      case 'green': colorClass += 'btn--color-green'; break;
-      default: colorClass += 'btn-white';
-    }
-
-    return (
-      <button
-        onClick={onClick}
-        type={type}
-        className={['btn' + colorClass + variantClass + className].join(' ')}
-      >
-        {children}
-      </button>
-    );
   }
-}
-
+  return <Button />;
+});
 Button.defaultProps = {
-  onClick: null,
   type: 'button',
-  className: '',
-  variant: 'm',
-  color: 'white',
+  className: null,
 }
-
 Button.propTypes = {
   children: PropTypes.node.isRequired,
-  onClick: PropTypes.func,
+  onClick: PropTypes.func.isRequired,
   type: PropTypes.oneOf(['button', 'submit', 'reset']),
   className: PropTypes.string,
-  variant: PropTypes.oneOf(['m', 'l']), // s = small, l = large
-  color: PropTypes.oneOf(['green', 'white'])
 }
-
 
 export default Button;
